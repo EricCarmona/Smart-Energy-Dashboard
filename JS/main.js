c0a1e62f15b06d5a6e2412d64e2202bb17ec8b1e
ERIC <ericcarcaspin@gmail.com>
2025-06-18 09:54:12 +0200

    Commit Css en Js

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+/*
+  Dit bestand maakt deel uit van de map 'js/' waarin alle
+  JavaScript-bestanden van het project zijn verzameld.
+  Hier worden de belangrijkste functies behandeld zoals:
+  - Beheer van energiedata
+  - Dynamische update van het dashboard
+  - Interactie met grafieken (Chart.js)
+  Het overzichtelijk houden van de code maakt lezen en onderhoud makkelijker.
+*/
